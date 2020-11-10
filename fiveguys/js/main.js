edaf733c8293696313e7d7faa6ad396bc1814ee6
seyidudu <59752314+seyidudu@users.noreply.github.com>
2020-11-09 20:36:31 -0700

    new commit

---
+++ fiveguys/js/main.js
@@ -0,0 +1,15 @@
+$(".menu-btn").click(function(e){
+    $(".menu-btn .fas").toggleClass("fa-bars fa-times-circle");
+
+
+    $("nav ul").toggle();
+    e.preventDefault();
+})
+
+
+    $(".choose-locations, .locations a").click(function(e){
+        $(".overlay").toggle();
+        e.preventDefault();
+    
+
+})
